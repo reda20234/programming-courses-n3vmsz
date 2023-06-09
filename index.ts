415de2a88c233a1d1c7c75748e16113831e7bed4
reda20234 <136035388+reda20234@users.noreply.github.com>
2023-06-09 02:08:38 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('reda benaad mrakech morocco');
+console.log('soulaiman ait oumghar');
+HTMLAllCollection.toString('reda benaad');
+HTMLAllCollection.toString('reda benaad');
